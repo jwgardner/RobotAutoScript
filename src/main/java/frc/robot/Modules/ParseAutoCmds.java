12dff8558c5e2dee30666305a64d26f581fd2aff
jwgardner <tsetsefly42@gmail.com>
2021-04-09 09:21:51 -0600

    update

--- src/main/java/frc/robot/Modules/ParseAutoCmds.java
+++ src/main/java/frc/robot/Modules/ParseAutoCmds.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 public class ParseAutoCmds extends SequentialCommandGroup {
     
-    SequentialCommandGroup m_AutoCommands = new  SequentialCommandGroup();
+    static SequentialCommandGroup m_AutoCommands = new  SequentialCommandGroup();
 
     public static SequentialCommandGroup parseAutoCmds () {
         String tempScript;
